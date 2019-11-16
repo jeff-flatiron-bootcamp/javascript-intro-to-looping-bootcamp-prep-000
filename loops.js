90dca60a5eea42885af220e6260c00cfae588ffa
Jeff Simon <jeffsbillinfo@gmail.com>
2019-11-16 02:45:10 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ function forLoop(array)
   for(let i = 0; i < 25; i++)
   {
     //console.log(`"I am ${i} stange loop${i===0 ? '' : 's' }."`);
-    array.push(`I am ${i} strange loop${i===0 ? '' : 's' }.`);
+    array.push(`I am ${i} strange loop${i===1 ? '' : 's' }.`);
   }
   return array;
 }
